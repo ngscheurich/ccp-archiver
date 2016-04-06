c49a1bc1573f3b3511a8df9c9985f83126e51a21
Nicholas Scheurich <nick@scheurich.me>
2016-04-06 18:40:12 -0500

    Add app menu; change from ERB to Haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def side_nav_class(nav_item, has_children = true)
+    klass = "" + nav_item
+    klass << " has-children" if has_children
+    klass << " active" if active_page?(nav_item)
+    klass
+  end
+
+  def active_page?(klass)
+    controller = body_class.split[0]
+    page = controller == "dashboards" ? "dashboard" : controller
+    page == klass
+  end
 end
