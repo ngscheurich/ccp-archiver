8c7adcc6b00c147690d4c046a4275e89d92b1d77
Nicholas Scheurich <nick@scheurich.me>
2016-04-13 15:49:15 -0500

    Refactor

--- lib/api_request.rb
+++ lib/api_request.rb
@@ -1,41 +1,40 @@
 require "benchmark"
 
-module ApiRequest
-  def self.dti(start_id = 1, num_stories = 1)
+class ApiRequest
+  attr_reader :api_response
+
+  def initialize(start_id = 1, num_stories = 1)
     dti = Dti.new(start_id, num_stories)
 
-    time_start = Time.now
-    response = dti.stories
-    time_finish = Time.now
+    @time_start = Time.now
+    @response = dti.stories
+    @time_finish = Time.now
 
-    create_api_response(
-      initiated_at: time_start,
-      elapsed_time: (time_finish - time_start) * 1000,
-      code: response.code,
-      message: response.message,
-      headers: response.headers.to_json,
-      body: response.to_json
+    @api_response = create_api_response
+    log_results(@api_response.save)
+  end
+
+  private
+
+  def create_api_response
+    @api_response = ApiResponse.new(
+      initiated_at: @time_start,
+      elapsed_time: (@time_finish - @time_start) * 1000,
+      code: @response.code,
+      message: @response.message,
+      headers: @response.headers.to_json,
+      body: @response.to_json
     )
   end
 
-  private_class_method def self.create_api_response(args)
-    api_response = ApiResponse.new(
-      initiated_at: args[:initiated_at],
-      elapsed_time: args[:elapsed_time],
-      code: args[:code],
-      message: args[:message],
-      headers: args[:headers],
-      body: args[:body]
-    )
+  def log_results(saved)
+    selected_attrs = %w(id code message initiated_at elapsed_time)
+    attrs = @api_response.attributes.select { |k| selected_attrs.include?(k) }
 
-    if api_response.save
-      Rails.logger.info("Saved ApiResponse: #{log_attrs(api_response)}")
+    if saved
+      Rails.logger.info("Saved ApiResponse: #{attrs}")
     else
-      Rails.logger.error("Couldn't save ApiResponse: #{log_attrs(api_response)}")
+      Rails.logger.error("Couldn't save ApiResponse: #{attrs}")
     end
-  end
-
-  private_class_method def self.log_attrs(api_response)
-    api_response.attributes.slice("id", "code", "message", "initiated_at", "elapsed_time")
   end
 end
