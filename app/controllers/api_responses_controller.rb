9cb0b38fb1da73bb25f496beec9c1b5d1b57180d
Nicholas Scheurich <nick@scheurich.me>
2016-04-19 13:17:29 -0500

    Paginate API responses

--- app/controllers/api_responses_controller.rb
+++ app/controllers/api_responses_controller.rb
@@ -2,7 +2,7 @@ class ApiResponsesController < ApplicationController
   before_action :require_login
 
   def index
-    @api_responses = ApiResponse.all
+    @api_responses = ApiResponse.paginate(page: params[:page], per_page: 25)
   end
 
   def show
