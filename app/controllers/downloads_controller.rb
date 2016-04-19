ebc4f008414c4a3c5223e67a978af260077cd86e
Nicholas Scheurich <nick@scheurich.me>
2016-04-19 13:22:11 -0500

    Disable downloading

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,21 +1,26 @@
 require "zip"
 
 class DownloadsController < ApplicationController
+  before_action :render_nothing
   before_action :require_login
   before_action :set_stories, only: :start
 
   def start
-    buffer = Zip::OutputStream.write_buffer do |zos|
-      @stories.each do |story|
-        zos.put_next_entry "article-#{story.cms_id}.xml"
-        zos.puts Nitf::Story.new(story).xml
-      end
-    end
+    # buffer = Zip::OutputStream.write_buffer do |zos|
+    #   @stories.each do |story|
+    #     zos.put_next_entry "article-#{story.cms_id}.xml"
+    #     zos.puts Nitf::Story.new(story).xml
+    #   end
+    # end
 
-    send_zip_file(buffer.string.force_encoding("binary"))
+    # send_zip_file(buffer.string.force_encoding("binary"))
   end
 
   private
+
+  def render_nothing
+    render nothing: true
+  end
 
   def set_stories
     @stories = Story.all
