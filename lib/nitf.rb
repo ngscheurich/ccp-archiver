9361a9773172ec4aa0f0acf1720928219ca81526
Nicholas Scheurich <nick@scheurich.me>
2016-05-04 11:51:18 -0500

    Remove weird CDATA sections from <script> tags

--- lib/nitf.rb
+++ lib/nitf.rb
@@ -108,7 +108,7 @@ ERROR
     def nitf_body_text(xml)
       if content?(@story.body)
         xml.block(xmlns: "http://www.w3.org/1999/xhtml") do
-          xml.<< @story.body.gsub(%r{<script type=\"text\/javascript\">.*<\/script>}, "")
+          xml .<< @story.body.gsub(%r{\/\/ <!\[CDATA.*\]\]>}, "")
         end
       end
     end
